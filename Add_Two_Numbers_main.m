205c08da6b2a7cd90a36f152c957bc535f407da6
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-15 18:04:53 +0800

    Create Add_Two_Numbers_main.m

---
+++ Add_Two_Numbers_main.m
@@ -0,0 +1,11 @@
+num1 = [2,4,3];
+num2 = [5,6,4];
+output = add_two_numbers(num1,num2)
+
+num1 = [0];
+num2 = [0];
+output = add_two_numbers(num1,num2)
+
+num1 = [9,9,9,9,9,9,9];
+num2 = [9,9,9,9];
+output = add_two_numbers(num1,num2)
