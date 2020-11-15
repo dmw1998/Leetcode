e37280362b84ad6899fd37edf8c9fdb0b58eccd4
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-15 18:09:33 +0800

    Update Add_Two_Numbers_main.m

--- Add_Two_Numbers_main.m
+++ Add_Two_Numbers_main.m
@@ -1,3 +1,8 @@
+% You are given two non-empty linked lists representing two non-negative integers. 
+% The digits are stored in reverse order, and each of their nodes contains a single digit. 
+% Add the two numbers and return the sum?as a linked list.
+% You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
 num1 = [2,4,3];
 num2 = [5,6,4];
 output = add_two_numbers(num1,num2)
