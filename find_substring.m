2fd2dfd0caa1b9474aa36aaed7cff9c29ee29c5d
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-16 18:03:48 +0800

    Create find_substring.m

---
+++ find_substring.m
@@ -0,0 +1,26 @@
+function [substring, len] = find_substring(str)
+
+n = strlength(str);
+len = 0;
+
+for i = 1 : n
+    string(1,i) = str(i);
+end
+
+for i = 1 : n
+    temp = string(i);
+    length = 1;
+    for j = i+1 : n
+        if contains(temp,string(j))
+            if length > len
+                len = length;
+                substring = temp;
+            end
+            break
+        else
+            temp = join([temp,string(j)],"");
+            length = length + 1;
+        end
+    end
+end
+end
