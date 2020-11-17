8336772a4853861a7437af23fa788890fafb7f59
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-17 09:59:56 +0800

    Create Longest_Palindromic_Substring.m

---
+++ Longest_Palindromic_Substring.m
@@ -0,0 +1,6 @@
+% Given a string s, return the longest palindromic substring in s.
+
+prompt = 'Please input a string: ';
+str = input(prompt,'s');
+
+substring = find_longest_palindromic_substring(str)
