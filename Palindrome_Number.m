31ba96a6abcc1c152bd9a086cbdd53af7d3020d5
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-18 16:49:32 +0800

    Create Palindrome_Number.m

---
+++ Palindrome_Number.m
@@ -0,0 +1,15 @@
+% Determine whether an integer is a palindrome. An integer is a palindrome
+% when it reads the same backward as forward.
+% Solve it without converting the integer to a string.
+
+x = 121
+if_palindrome_num(x)
+
+x = -121
+if_palindrome_num(x)
+
+x = 10
+if_palindrome_num(x)
+
+x = -101
+if_palindrome_num(x)
