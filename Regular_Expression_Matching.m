5fc8429219d101dc798aa0d489f0cd48456ef73d
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-19 17:46:05 +0800

    Create Regular_Expression_Matching.m

---
+++ Regular_Expression_Matching.m
@@ -0,0 +1,13 @@
+% Given an input string (s) and a pattern (p), implement regular expression
+% matching with support for '.' and '*' where:
+% '.' Matches any single character.
+% '*' Matches zero or more of the preceding element.
+% The matching should cover the entire inout string (not partial).
+
+result = match_patter();
+
+if result
+    fprintf('true\r')
+else
+    fprintf('false\r')
+end
