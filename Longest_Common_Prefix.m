1ee6ce65a9474cd0cb5099e78970dd11bd0cc2e3
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-21 15:46:40 +0800

    Create Longest_Common_Prefix.m

---
+++ Longest_Common_Prefix.m
@@ -0,0 +1,9 @@
+% Write a function to find the longest common prefix string amoungst an
+% array of strings.
+% If there is no common prefix, return an empty string "".
+
+strs = ["flower", "flow", "flight"];
+str = find_longest_common_prefix(strs)
+
+strs = ["dog", "racecar", "car"];
+str = find_longest_common_prefix(strs)
