39e11723b37f5e515ec68657bea24a576fcea131
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-22 18:38:55 +0800

    Create Three_Sum_Closest.m

---
+++ Three_Sum_Closest.m
@@ -0,0 +1,8 @@
+% Given an array nums of n integers and an integer target, find three
+% integers in nums such that the sum is closest to target. Return thee sum
+% of the three integers. You may assume that each inout would have exactly
+% one solution.
+
+nums = [-1, 2, 1, 4];
+target = 1;
+output  = find_target_sum_triplet(nums,target)
