df45eb5a020c03d9168d4c8ca394ee1c5d32e66d
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-25 20:02:48 +0800

    Create Generate_Parentheses.m

---
+++ Generate_Parentheses.m
@@ -0,0 +1,11 @@
+% Given n pairs of parentheses, write a function to generate all
+% combinations of well-formed parentheses.
+
+n = 3;
+output = generate_n_parentheses(n)
+
+n = 1;
+output = generate_n_parentheses(n)
+
+n = 8;
+output = generate_n_parentheses(n)
