f64bfd7f416accf49e1ad03c1667673fb06e2792
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-26 19:33:23 +0800

    Update Merge_Two_Sorted_Lists.m

--- Merge_Two_Sorted_Lists.m
+++ Merge_Two_Sorted_Lists.m
@@ -2,21 +2,29 @@
 % list should be made by splicing together the nodes of the first two
 % lists.
 
+% Create two sorted linked lists
 l1 = dlnode(1);
 n2 = dlnode(2);
 n3 = dlnode(4);
 
 n2.insertAfter(l1);
-
-temp = n2;
-n3.insertAfter(temp);
-
-n2.Next
-
-n2 = dlnode(3);
-l2 = dlnode(1);
-
-n2.insertAfter(l2);
 n3.insertAfter(n2);
 
-% output = merge_two_lists(l1,l2)
+n4 = dlnode(3);
+n5 = dlnode(4);
+l2 = dlnode(1);
+
+n4.insertAfter(l2);
+n5.insertAfter(n4);
+
+l1_data = [l1.Data, l1.Next.Data, l1.Next.Next.Data];
+l2_data = [l2.Data, l2.Next.Data, l2.Next.Next.Data];
+
+% Print result
+output = merge_two_lists(l1,l2);
+temp = output;
+for i = 1 : 6
+    data_set(i) = temp.Data;
+    temp = temp.Next;
+end
+data_set
