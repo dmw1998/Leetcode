27a2a9b023629d530c8a2bc2c11e68222d3f1e9b
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-26 19:33:16 +0800

    Update merge_two_lists.m

--- merge_two_lists.m
+++ merge_two_lists.m
@@ -1,27 +1,35 @@
 function lresult = merge_two_lists(l1,l2)
 
-if l1 == null
+if isnan(l1.Data) 
     lresult = l2;
-elseif l2 == null
+    return
+elseif isnan(l2.Data)
     lresult = l1;
+    return
 elseif l1.Data < l2.Data
     lresult = l1;
+    temp = l1.Next;
     pointer = l2;
 else
     lresult = l2;
+    temp = l2.Next;
     pointer = l1;
 end
 
-temp = lresult.Next;
+node = lresult;
 
-while pointer.Data
-    if temp.Data > pointer.Data
-        pointer.insertAfter(temp)
-        temp1 = temp;
-        temp = pointer.Next;
+for i = 1 : 4           % 4 = length of pointer link + length of temp link - 2
+    if temp.Data >= pointer.Data
+        temp1 = pointer.Next;
+        pointer.insertAfter(node)
         pointer = temp1;
+        node = node.Next;
     else
-        temp = temp.Next;
+        temp2 = temp.Next;
+        temp.insertAfter(node)
+        temp = temp2;
+        node = node.Next;
     end
-        
+end
+
 end
