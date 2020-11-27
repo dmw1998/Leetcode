9afd50181f49e55a4f711ef86abbb980b882b80a
dmw1998 <dmw1998@users.noreply.github.com>
2020-11-27 19:56:18 +0800

    Update Swap_Nodes_in_Pairs.m

--- Swap_Nodes_in_Pairs.m
+++ Swap_Nodes_in_Pairs.m
@@ -12,16 +12,8 @@ for i = 2 : 4
 end
 
 %% Swap every two adjacent nodes
-node = head;
-node1 = node;
-node2 = node1.Next;
-swap_nodes(node1,node2)
-head = node2;
 
-node = node.Next.Next;
-node1 = node;
-node2 = node1.Next;
-swap_nodes(node1,node2)
+head = swap_nodes(head);
 
 %% Return its head
 output = [head.Data, head.Next.Data, head.Next.Next.Data, head.Next.Next.Next.Data]
