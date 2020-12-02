bdb059e1f36bd3885077f95002269c0920428cd6
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-02 22:47:19 +0800

    Create Length_of_Last_Word.m

---
+++ Length_of_Last_Word.m
@@ -0,0 +1,10 @@
+% Given a string s consists of some words separated by spaces, return the
+% length of the last wordd in the string. If the last word does not exist,
+% return 0.
+% A word is a maximal substring consisting of non-space characters only.
+
+s = "Hello World";
+output = find_length_of_last_word(s)
+
+s = " ";
+output = find_length_of_last_word(s)
