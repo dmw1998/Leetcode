ca62a9df86975424f241c1fda8c45755d7b38cf5
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-03 21:12:03 +0800

    Create Trapping_Rain_Water.m

---
+++ Trapping_Rain_Water.m
@@ -0,0 +1,8 @@
+% Given n non-negative integers representing an elevation map where the
+% width of each bar is 1, compute how much water it can trap raining.
+
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+output = trapping_water(height)
+
+height = [4, 2, 0, 3, 2, 5];
+output = trapping_water(height)
