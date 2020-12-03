c133f7c254e7c0af3f2910b25fb65f2116701788
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-03 21:35:57 +0800

    Update Trapping_Rain_Water.m

--- Trapping_Rain_Water.m
+++ Trapping_Rain_Water.m
@@ -3,6 +3,8 @@
 
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 output = trapping_water(height)
+output = trapping_water1(height)
 
 height = [4, 2, 0, 3, 2, 5];
 output = trapping_water(height)
+output = trapping_water1(height)
