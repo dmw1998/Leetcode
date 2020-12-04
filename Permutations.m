affc6ba98313526482332ce2f3e80b5497a19e09
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-04 20:52:05 +0800

    Create Permutations.m

---
+++ Permutations.m
@@ -0,0 +1,11 @@
+% Given an array nums of distinct integers, return all the possible
+% permutations. You can return the answer in any order.
+
+nums = [1, 2, 3];
+output = permute_dis_nums(nums)
+
+nums = [0, 1];
+output = permute_dis_nums(nums)
+
+nums = [1];
+output = permute_dis_nums(nums)
