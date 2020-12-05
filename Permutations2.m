37490a4f7f56d1db160795c2d4d0fb149387ec5c
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-05 22:09:27 +0800

    Create Permutations2.m

---
+++ Permutations2.m
@@ -0,0 +1,13 @@
+% Given a collection of numbers, nums, that might contain duplicates,
+% return all possible unique permutations in any order.
+
+nums = [1, 1, 2];
+output = permute_nums(nums)
+
+nums = [1, 2, 3];
+output = permute_nums(nums)
+
+tic
+nums = [1, 2, 3, 4, 5, 6, 7, 8];
+output = permute_nums(nums)
+toc
