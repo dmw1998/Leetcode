5bb95f3f45e2f29eb0ec4a10e4eab4d7e4919280
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-07 16:18:29 +0800

    Create Pow_x_n.m

---
+++ Pow_x_n.m
@@ -0,0 +1,13 @@
+% Implement pow(x,n), which calculates x raised to the power n (i.e. n^n).
+
+x = 2.00000;
+n = 10;
+output = pow(x,n)
+
+x = 2.10000;
+n = 3;
+output = pow(x,n)
+
+x = 2.00000;
+n = -2;
+output = pow(x,n)
