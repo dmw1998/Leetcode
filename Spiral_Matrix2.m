0292a194caacf5fc6e452e30d9744b8c15a7d584
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-08 18:48:42 +0800

    Create Spiral_Matrix2.m

---
+++ Spiral_Matrix2.m
@@ -0,0 +1,8 @@
+% Given a positive integer n, generate an n by n matrix filled with
+% elements from 1 to n^2 in spiral order.
+
+n = 3;
+output = print_spiral_matrix2(n)
+
+n = 1;
+output = print_spiral_matrix2(n)
