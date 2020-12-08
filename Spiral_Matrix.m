8d6b19d59f2338f64dfe928a04df2d1d36b04d75
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-08 18:12:30 +0800

    Create Spiral_Matrix.m

---
+++ Spiral_Matrix.m
@@ -0,0 +1,8 @@
+% Given an m by n matrix, return all elements of the matrix in spiral
+% order.
+
+matrix = [1, 2, 3; 4, 5, 6; 7, 8, 9];
+output = print_spiral_matrix(matrix)
+
+matrix = [1, 2, 3, 4; 5, 6, 7, 8; 9, 10, 11, 12];
+output = print_spiral_matrix(matrix)
