af4ebf675113a9a21991f97b2556d33003097ebc
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-10 22:32:15 +0800

    Create Merge_Sorted_Array.m

---
+++ Merge_Sorted_Array.m
@@ -0,0 +1,11 @@
+% Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1
+% as one sorted array.
+% Note:
+% The number of elements initialized in nums1 and nums2 are m and n
+% respectively.
+% You may assume that nums1 has enough space (size that is equal to m + n)
+% to hold additional elements from nums2.
+
+nums1 = [1, 2, 3];
+nums2 = [2, 5, 6];
+output = merge_arrays(nums1,nums2)
