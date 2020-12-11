89234a656e20fcc8e5a27a4e3022688a0ed8d7a6
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-11 20:50:55 +0800

    Create Best_Time_to_Buy_and_Sell_Stock2.m

---
+++ Best_Time_to_Buy_and_Sell_Stock2.m
@@ -0,0 +1,16 @@
+% Say you have an array prices for which the ith element is the price of a
+% given stock on day i.
+% Design an algorithm to find the maximum profit. You may conplete as many
+% transactions as you like (i.e., buy one and sell one share of the stock
+% multiple times).
+% Note: You may not engage in multiple transctions at the same time (i.e.,
+% you must sell the stock before you buy again).
+
+input = [7, 1, 5, 3, 6, 4];
+output = find_largest_profit2(input)
+
+input = [1, 2, 3, 4, 5];
+output = find_largest_profit2(input)
+
+input = [7, 6, 4, 3, 1];
+output = find_largest_profit2(input)
