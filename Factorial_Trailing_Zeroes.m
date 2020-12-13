b3b53e84bf63aab8a8692c9fce0b0f687f4f6d00
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-13 20:45:24 +0800

    Create Factorial_Trailing_Zeroes.m

---
+++ Factorial_Trailing_Zeroes.m
@@ -0,0 +1,12 @@
+% Given an integer n, return the number of trailing zzeroes in n!.
+% Follow up: Could you write a solution that works in logarithmic time
+% complexity?
+
+n = 3;
+output = find_trailing_zeroes(n)
+
+n = 5;
+output = find_trailing_zeroes(n)
+
+n = 0;
+output = find_trailing_zeroes(n)
