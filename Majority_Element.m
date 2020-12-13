ba55bd4c9229a576b3dbffc670d0e6466bfcfb72
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-13 20:45:15 +0800

    Create Majority_Element.m

---
+++ Majority_Element.m
@@ -0,0 +1,10 @@
+% Given an array of size n, find the majority element. The majority element
+% is the element that appears more than [n/2] times.
+% You may assume that the array is non-empty and the majority element
+% always exist in the array.
+
+input = [3, 2, 3];
+output = find_majority_element(input)
+
+input = [2, 2, 1, 1, 1, 2, 2];
+output = find_majority_element(input)
