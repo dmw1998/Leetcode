c5fb82446464ae69f6400995ac6996a6c6e82f58
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-14 20:41:52 +0800

    Create Count_Primes.m

---
+++ Count_Primes.m
@@ -0,0 +1,10 @@
+% Count the number of prime numbers less than a non-negative number n.
+
+n = 10;
+output = num_of_primes(n)
+
+n = 0;
+output = num_of_primes(n)
+
+n = 1;
+output = num_of_primes(n)
