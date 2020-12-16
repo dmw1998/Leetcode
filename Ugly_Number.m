d76b05b6b4095dc506c1f4fb92efcd63d0363ff4
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-16 20:50:30 +0800

    Create Ugly_Number.m

---
+++ Ugly_Number.m
@@ -0,0 +1,12 @@
+% Write a program to check whether a given number is an ugly number.
+% Ugly numbers are positive numbers whose prime factors only include 2, 3,
+% 5.
+
+input = 6;
+output = isUglyNumber(input)
+
+input = 8;
+output = isUglyNumber(input)
+
+input = 14;
+output = isUglyNumber(input)
