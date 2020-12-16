a2125a44bbd24fdb174df3bb32895c056efaddf6
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-16 20:58:09 +0800

    Create Ugly_Number2.m

---
+++ Ugly_Number2.m
@@ -0,0 +1,9 @@
+% Write a program to find the n-th ugly number.
+% Ugly numbers are positive numbers whose prime factor only include 2, 3,
+% 5.
+
+n = 10;
+output = find_nth_ugly_number(n)
+
+n = 1;
+output = find_nth_ugly_number(n)
