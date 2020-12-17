4b90c84d19148c94a91a1505de4f50b432a7a843
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-17 21:26:05 +0800

    Create Perfect_Squares.m

---
+++ Perfect_Squares.m
@@ -0,0 +1,8 @@
+% Given a positive integer n, find the least number of perfect square
+% numbers (for example, 1, 4, 8, 16, ... ) which sum to n.
+
+n = 12;
+output = find_num_of_squares(n)
+
+n = 13;
+output = find_num_of_squares(n)
