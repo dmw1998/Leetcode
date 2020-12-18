bb15fc4f9de42f674e7d9b58dc59d45a43cf656a
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-18 20:31:44 +0800

    Create Move_Zeros.m

---
+++ Move_Zeros.m
@@ -0,0 +1,8 @@
+% Given an array nums, write a function to move all 0's to the end of it
+% while maintaining the relative order of the non-zero elements.
+% Note:
+% 1. You must do this in-place without making a copy of the array.
+% 2. Minimize the total number of operations.
+
+input = [0, 1, 0, 3, 12];
+output = move_zero(input)
