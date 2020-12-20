bc042384af2bc8e4ccd751b933b1ea958c28d307
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-20 20:02:31 +0800

    Create Word_Pattern.m

---
+++ Word_Pattern.m
@@ -0,0 +1,19 @@
+% Given a pattern and a string s, find if s follows the same pattern.
+% Here follow means a full match, that there is a bijection betweeen a
+% letter in pattern and a non-empty word in s.
+
+pattern = "abba";
+s = "dog cat cat dog";
+output = if_mathch_pattern(pattern, s)
+
+pattern = "abba";
+s = "dog cat cat fish";
+output = if_mathch_pattern(pattern, s)
+
+pattern = "aaaa";
+s = "dog cat cat dog";
+output = if_mathch_pattern(pattern, s)
+
+pattern = "abba";
+s = "dog dog dog dog";
+output = if_mathch_pattern(pattern, s)
