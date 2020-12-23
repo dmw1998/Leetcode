fa55fe598235cfc87e73d7529c01958e7a4a9e97
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-23 20:43:38 +0800

    Create Super_Ugly_Number.m

---
+++ Super_Ugly_Number.m
@@ -0,0 +1,7 @@
+% Write a program to find the nth super ugly number.
+% Super ugly number are positive numbers whose all prime factor are in the
+% given prime list primes of size k.
+
+n = 12;
+primes = [2, 7, 13, 19];
+output = find_nth_super_ugly_num(n,primes)
