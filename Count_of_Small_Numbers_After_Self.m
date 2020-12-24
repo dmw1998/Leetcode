c6c7dc6655f4007215482b21dae36fc7b62c48ba
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-24 21:47:33 +0800

    Create Count_of_Small_Numbers_After_Self.m

---
+++ Count_of_Small_Numbers_After_Self.m
@@ -0,0 +1,6 @@
+% You are given an integer array nums and you have to return a new counts
+% array. The counts array has the property where counts[i] is the number of
+% smaller elements to the right of nums[i].
+
+nums = [5, 2, 6, 1]
+output = count_smaller_after(nums)
