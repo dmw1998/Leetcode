d6f6ebc9e5a16aa4f3f799a3147daddf7e597ed3
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-24 21:48:22 +0800

    Create Remove_Duplicate_Letters.m

---
+++ Remove_Duplicate_Letters.m
@@ -0,0 +1,9 @@
+% Given a string s, remove duplicate letters so that every letter appears
+% once and only once. You must make sure your result is the smallest in
+% lexicographical order among all possible results.
+
+s = "bcabc";
+output = RemoveDuplicateLetters(s)
+
+s = "cbacdcbc";
+output = RemoveDuplicateLetters(s)
