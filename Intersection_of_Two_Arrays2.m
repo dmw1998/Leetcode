f5acc022f24b7446b1ac039cc203b973ed3c797f
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-26 20:20:54 +0800

    Create Intersection_of_Two_Arrays2.m

---
+++ Intersection_of_Two_Arrays2.m
@@ -0,0 +1,9 @@
+% Given two arrays, write a function to compute their intersection.
+
+nums1 = [1, 2, 2, 1];
+nums2 = [2, 2];
+output = intersection2(nums1,nums2)
+
+nums1 = [4, 9, 5];
+nums2 = [9, 4, 9, 8, 4];
+output = intersection2(nums1,nums2)
