c8a3a99a78d4f0774ef8c9f5c99715bee32056e7
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-28 16:40:26 +0800

    Create Super_Pow.m

---
+++ Super_Pow.m
@@ -0,0 +1,18 @@
+% Your task is to calculate a^b mod 1337 where a is a positive integer and
+% b is an extremely large positive integer given in the form of an array.
+
+a = 2;
+b = [3];
+output = superPow(a,b)
+
+a = 2;
+b = [1,0];
+output = superPow(a,b)
+
+a = 1;
+b = [4, 3, 3, 8, 5, 2];
+output = superPow(a,b)
+
+a = 2147483647;
+b = [2, 0, 0];
+output = superPow(a,b)
