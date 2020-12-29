1cb15114246092f2737f423ad6b7156a67a51073
dmw1998 <dmw1998@users.noreply.github.com>
2020-12-29 21:26:40 +0800

    Create Combination_Sum5.m

---
+++ Combination_Sum5.m
@@ -0,0 +1,11 @@
+% Given an integer array eith all positive numbers and no duplicates, find
+% the number of possible combinations that add up to a positive integer
+% target.
+% Follow up:
+% What if negative numbers are allowed in the given array?
+% How does it change the problem?
+% What limitation we need to add to the question to allow negative numbers?
+
+nums = [1, 2, 3];
+target = 4;
+output = find_combination_sum5(nums, target)
