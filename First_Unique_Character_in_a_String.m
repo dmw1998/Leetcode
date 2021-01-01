c7cdc2bf1b259e51d3f45160d2f1bee0e7cf4207
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-01 20:16:37 +0800

    Create First_Unique_Character_in_a_String.m

---
+++ First_Unique_Character_in_a_String.m
@@ -0,0 +1,8 @@
+% Given a string, find the first non-repeating character in it and return
+% its index. If it doesn't exist, return -1.
+
+s = "leetcode";
+output = firstUniqChar(s)
+
+s = "loveleetcode";
+output = firstUniqChar(s)
