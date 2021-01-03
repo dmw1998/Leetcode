b34a4391fe32d1ac660b33866cf553373588e434
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-03 20:26:00 +0800

    Create Longest_Substring_with_At_Least_K_Repeating_Characters.m

---
+++ Longest_Substring_with_At_Least_K_Repeating_Characters.m
@@ -0,0 +1,11 @@
+% Given a string s and an integer k, resutrn the length of the longest
+% substring of s such that the frequency of each character in this
+% substring is greater than or equal to k.
+
+s = 'aaabb';
+k = 3;
+output = longestSubstring(s,k)
+
+s = 'ababbc';
+k = 2;
+output = longestSubstring(s,k)
