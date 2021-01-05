9f98d26b16cd8da0ea9a5e1537047bd2f2d99a6e
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-05 19:27:54 +0800

    Create Nth_Digit.m

---
+++ Nth_Digit.m
@@ -0,0 +1,11 @@
+% Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7,
+% 8, 9, 10, 11, ...
+% Note:
+% n is positive and will fit within the range of a 32-bit signed integer (n
+% < 2^31).
+
+input = 3;
+output = findNthDigit(input)
+
+input = 11;
+output = findNthDigit(input)
