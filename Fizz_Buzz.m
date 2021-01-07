ed69a6cbc380dd8d308fb1afdeff9ce975be8a1e
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-07 21:01:46 +0800

    Create Fizz_Buzz.m

---
+++ Fizz_Buzz.m
@@ -0,0 +1,8 @@
+% Write a program that ooutput the string representation of numbers from 1
+% to n.
+% But for multiples of three it should output "Fizz" instead of the number
+% and for the multiples of five output "Buzz". For numbers which are
+% multiples of both three and five out put "FizzBuzz".
+
+n = 15;
+output = fizzBuzz(n)
