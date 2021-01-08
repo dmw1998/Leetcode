919c65575c55c132931d60e85104688d11c88e52
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-08 19:37:19 +0800

    Create Third_Maximum_Number.m

---
+++ Third_Maximum_Number.m
@@ -0,0 +1,11 @@
+% Given integer array nums, return the third maximum number in this array.
+% If the third maximum does not exist, return the maximum number.
+
+nums = [3, 2, 1];
+output = thirdMax(nums)
+
+nums = [1,2];
+output = thirdMax(nums)
+
+nums = [2, 2, 3, 1];
+output = thirdMax(nums)
