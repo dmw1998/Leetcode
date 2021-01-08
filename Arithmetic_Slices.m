7af3d2fd66fc1222c9d27f67a08bc5b311fa74ab
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-08 19:37:32 +0800

    Create Arithmetic_Slices.m

---
+++ Arithmetic_Slices.m
@@ -0,0 +1,6 @@
+% A sequence of numbers is called arithmetic if it consists of at least
+% three elements and if the difference between any consecutive element is
+% the same.
+
+A = [1, 2, 3, 4];
+output = numberOfArithmeticSlices(A)
