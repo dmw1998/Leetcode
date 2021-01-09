eca1bca43a3ce574fdcfb2f9200a22fabcf0e000
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-09 20:25:16 +0800

    Create Find_the_Given_Sum.m

---
+++ Dynamic_Programming/Find_the_Given_Sum.m
@@ -0,0 +1,9 @@
+% Given a sum and an array, find wether the sum of numbers in the array can
+% be equal to given summation.
+% Note: All numbers are positive.
+
+nums = [3, 34, 4, 12, 5, 2];
+s = 9;
+output = subset(nums, s)
+s = 13;
+output = subset(nums, s)
