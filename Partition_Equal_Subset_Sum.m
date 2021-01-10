11f3d4c4e6f721b4af50a7d7e830497375d5e6de
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-10 22:10:46 +0800

    Create Partition_Equal_Subset_Sum.m

---
+++ Partition_Equal_Subset_Sum.m
@@ -0,0 +1,9 @@
+% Given a non-empty array nums containing only positive integers, find if
+% the array can be partitioned into two subset such that the sum of
+% elements in both subsets is equal.
+
+nums = [1, 5, 11, 5];
+output = canPartition(nums)
+
+nums = [1, 2, 3, 5];
+output = canPartition(nums)
