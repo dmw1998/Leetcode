6e92d1f8dc3b8b3739d249f9f888361abbb0223c
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-10 22:10:27 +0800

    Create Add_Strings.m

---
+++ Add_Strings.m
@@ -0,0 +1,16 @@
+% Given two non-negative integers num1 and num2 represented as string,
+% return the sum of num1 and num2.
+% Note:
+% 1. The length of both num1 and num2 is < 5100.
+% 2. Both num1 and num2 contains only 0-9.
+% 3. Both num1 and num2 does not contain any leading zero.
+% 4. You must not use any built-in BigInteger library or convert the inputs
+% to integer directly.
+
+num1 = "3";
+num2 = "9";
+output = addString(num1, num2)
+
+num1 = "7777777";
+num2 = "88345679";
+output = addString(num1, num2)
