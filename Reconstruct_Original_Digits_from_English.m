eb97e2a71097388042135dd7f1d4e64af539d19e
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-12 21:29:16 +0800

    Create Reconstruct_Original_Digits_from_English.m

---
+++ Reconstruct_Original_Digits_from_English.m
@@ -0,0 +1,14 @@
+% Given a non-empty string containing an out-of-order English
+% representation of digits 0-9, output the digits in assending order.
+% Note:
+% 1. Input contains only lowercase English letters.
+% 2. Input is guaranteed to be valid and can be transformed to its original
+% digits. That means invalid inputs such as "abc" or "zerone" are not
+% permitted.
+% 3. Input length is less than 50,000.
+
+input = "owoztneoer";
+output = originalDigits(input)
+
+input = "fviefuro";
+output = originalDigits(input)
