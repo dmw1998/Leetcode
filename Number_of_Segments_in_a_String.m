311310b23d136710a567f697f9397e15e477ada9
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-15 19:47:12 +0800

    Create Number_of_Segments_in_a_String.m

---
+++ Number_of_Segments_in_a_String.m
@@ -0,0 +1,14 @@
+% You are given a string s, return the number of segments in the string.
+% A segment is defined to be a contiguous sequence of non-space characters.
+
+s = "Hello, my name is John";
+output = countSegments(s)
+
+s = "Hello";
+output = countSegments(s)
+
+s = "love live! mu'sic forever";
+output = countSegments(s)
+
+s = "";
+output = countSegments(s)
