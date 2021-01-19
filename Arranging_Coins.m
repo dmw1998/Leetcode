a5242070677fbcdaedc6bdc5993abf897ab6f098
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-19 19:06:38 +0800

    Create Arranging_Coins.m

---
+++ Arranging_Coins.m
@@ -0,0 +1,11 @@
+% You have a total of n coins that you want to form in a staircase shape,
+% where every k-th row must have exactly k coins.
+% Given n, find the total number of full staircase row that can be formed.
+% n is a non-negative integer and fits within the range of a 32-biit signed
+% integer.
+
+n = 5;
+output = arrangeCoins(n)
+
+n = 8;
+output = arrangeCoins(n)
