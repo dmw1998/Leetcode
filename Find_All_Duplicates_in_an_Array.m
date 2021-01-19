af7a2ff162f1dd04c48363c57da78ac2a947d053
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-19 20:58:21 +0800

    Create Find_All_Duplicates_in_an_Array.m

---
+++ Find_All_Duplicates_in_an_Array.m
@@ -0,0 +1,7 @@
+% Given an array of integers, 1 <= a[i] <= n (n = size of array), some
+% elements appear twice and others appear once.
+% Find all the elements that appear twice in this array.
+% Could you do it without extra space and in O(n) runtime?
+
+input = [4,3,2,7,8,2,3,1];
+output = findDuplicates(input)
