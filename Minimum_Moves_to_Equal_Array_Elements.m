a8ed14e3218ea697d713468dfd6331763ac58742
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-22 14:14:47 +0800

    Create Minimum_Moves_to_Equal_Array_Elements.m

---
+++ Minimum_Moves_to_Equal_Array_Elements.m
@@ -0,0 +1,9 @@
+% Given a non-empty integer array of size n, find the minimum number of
+% moves required to make all array elements equal, where a move is
+% incrementing n-1 elements by 1.
+
+array = [1, 2, 3];
+output = minMoves(array)
+
+array = [13, 18, 3, 10, 35, 68, 50, 20];
+output = minMoves(array)
