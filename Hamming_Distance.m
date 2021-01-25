5d7ddb12a3979208490dfb63d87bf7397d0f2779
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-25 14:17:44 +0800

    Create Hamming_Distance.m

---
+++ Hamming_Distance.m
@@ -0,0 +1,11 @@
+% The Hamming distance between two integers is the number of positions at
+% which the corresponding bits are different.
+% Given two integers x and y, calculate the Hamming distance.
+
+x = 1;
+y = 4;
+output = hammingDistance(x,y)
+
+x = 9;
+y = 12;
+output = hammingDistance(x,y)
