2267f21d320bdfbe4f8cd330d22e49e72c9db758
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-26 20:38:23 +0800

    Create Minimum_Moves_to_Equal_Array_Elements2.m

---
+++ Minimum_Moves_to_Equal_Array_Elements2.m
@@ -0,0 +1,10 @@
+% Given a non-empty integer array, find the minimum number of moves
+% required to make all array elements equal, where a move is incrementing a
+% selected element by 1 or decrementing a selected element by 1.
+% You may assume the array's length is at most 10,000.
+
+nums = [1, 2, 3];
+output =  minMoves2(nums)
+
+nums = [3, 8, 2, 5, 1, 4, 7, 6];
+output =  minMoves2(nums)
