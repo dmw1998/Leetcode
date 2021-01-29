a31da83ffd5e209b99c516066fe078b72ac50a5c
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-29 20:32:52 +0800

    Create Number_Complement.m

---
+++ Number_Complement.m
@@ -0,0 +1,8 @@
+% Given a positive integer num, output its complement number. The
+% complement strategy is to flip the bits of its binary representation.
+
+num = 5;
+output = findComplement(num)
+
+num = 1;
+output = findComplement(num)
