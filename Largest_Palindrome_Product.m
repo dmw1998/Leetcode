00c3dd46471db5ab9ec373fb6bd9ec2fb46126fc
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-30 18:32:19 +0800

    Create Largest_Palindrome_Product.m

---
+++ Largest_Palindrome_Product.m
@@ -0,0 +1,8 @@
+% Find the largest palindrome made from the product of two n-digit numbers.
+% Since the result could be very large, you should return the largest
+% palindrome mod 1337.
+
+for n = 1 : 8
+    input = n
+    output = largestPalindrome(input)
+end
