6e13da8b22dd64747027b974f9fd82ae250310f5
dmw1998 <dmw1998@users.noreply.github.com>
2021-01-30 15:44:22 +0800

    Create Total_Hamming_Distance.m

---
+++ Total_Hamming_Distance.m
@@ -0,0 +1,7 @@
+% The Hamming distance between two intergers is the number of positions at
+% which the corresponding bits are different.
+% Now your job is to find the total Hamming distance between all pairs of
+% the given numbers.
+
+nums = [4, 14, 2];
+output = totalHammingDistance(nums)
