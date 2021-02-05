dfaccc82f0945a61fdc8e925c9f39c8216c7f4be
dmw1998 <dmw1998@users.noreply.github.com>
2021-02-05 20:42:33 +0800

    Create Next_Greater_Element2.m

---
+++ Next_Greater_Element2.m
@@ -0,0 +1,9 @@
+% Given a circular array (the next element of the last element is the first
+% element of the array), print the Next Great Number for every element. The
+% Next Greater Number of a number x is the first greater number to its
+% traversing-order next in the array, which means you could search
+% circularly to find its next greater number. If it doesn't exist, output
+% -1 for this number.
+
+input = [1, 2, 1];
+output = nextGreaterElements(input)
