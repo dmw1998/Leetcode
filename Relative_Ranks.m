e2b120b5885423bbf19635d172791d4fe7172c76
dmw1998 <dmw1998@users.noreply.github.com>
2021-02-06 20:56:57 +0800

    Create Relative_Ranks.m

---
+++ Relative_Ranks.m
@@ -0,0 +1,6 @@
+% Given scores of N athletes, find their relative ranks and the people with
+% the top three highest scores, who will be awarded medals:"Gold Medal",
+% "Silver Medal" and "Bronze Medal".
+
+input = [5, 4, 3, 2, 1];
+output = findRelativeRanks(input)
