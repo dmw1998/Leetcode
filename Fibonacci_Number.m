fdbe167dc5ebbf1471a682cd15eb7b98265c7ad7
dmw1998 <dmw1998@users.noreply.github.com>
2021-02-07 06:09:23 +0800

    Create Fibonacci_Number.m

---
+++ Fibonacci_Number.m
@@ -0,0 +1,12 @@
+% The Fibonacci numbers, commonly denoted F(n) form a sequence, called the
+% Fibonacci sequence, such that each number is the sum of the two preceding
+% ones, starting from 0 and 1. 
+
+n = 2;
+output = fib(n)
+
+n = 3;
+output = fib(n)
+
+n = 4;
+output = fib(n)
