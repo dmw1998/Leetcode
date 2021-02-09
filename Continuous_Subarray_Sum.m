6974e086e26b7f185fad4c0eb178c679dd64830c
dmw1998 <dmw1998@users.noreply.github.com>
2021-02-09 22:14:55 +0800

    Create Continuous_Subarray_Sum.m

---
+++ Continuous_Subarray_Sum.m
@@ -0,0 +1,8 @@
+% Given a list of non-negative numbers and a target integer k, write a
+% function to check if the array has a continuous subarray of size at least
+% 2 that sums up to a multiple of k, that is, sums up to n*k where is also
+% an integer.
+
+nums = [23, 2, 4, 6, 7];
+k = 6;
+output = checkSubarraySum(nums, k)
