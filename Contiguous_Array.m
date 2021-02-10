a817bf9222f216dbc0569612ab5c1f1796fd974d
dmw1998 <dmw1998@users.noreply.github.com>
2021-02-10 21:25:08 +0800

    Create Contiguous_Array.m

---
+++ Contiguous_Array.m
@@ -0,0 +1,8 @@
+% Given a binary array, find the maximum length of a contiguous subarray
+% with equal number of 0 and 1.
+
+list = [0, 1];
+output = findMaxLength(list)
+
+list = [0, 1, 0];
+output = findMaxLength(list)
